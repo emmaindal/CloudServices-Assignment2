5ffb3207c5fd47d44ebe0b92440e2acf3dfa1477
emmaindal <emma.indahl@gmail.com>
2019-05-11 11:18:41 +0200

    created models

---
+++ project/app/Product.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model 
+{
+    protected $touches = ['stores'];
+    protected $appends = ['reviews', 'stores'];
+
+    public function getReviewsAttribute() {
+        return $this->hasMany('App\Review')->get();
+    }
+
+    public function getStoresAttribute() {
+        return $this->belongsToMany('App\Store')->get();
+    }
+
+    public function stores() {
+        return $this->belongsToMany('App\Store');
+    }
+    
+    public function reviews() {
+        return $this->hasMany('App\Review');
+    }
+}
